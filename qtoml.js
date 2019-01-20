b8e2f8a5a2d84b72db622173c3eeee3631099b5e
Rebecca Turner <me@re-becca.org>
2019-01-20 14:45:56 -0800

    chore: Switch to MY standard

--- qtoml.js
+++ qtoml.js
@@ -4,9 +4,9 @@ module.exports = function () {
   const args = Object.assign([], arguments[0].raw)
   const values = [].slice.call(arguments, 1)
   let toml = ''
-  while (args.length) {
+  while (args.length > 0) {
     toml += args.shift()
-    if (values.length) toml += TOML.stringify.value(values.shift())
+    if (values.length > 0) toml += TOML.stringify.value(values.shift())
   }
   return TOML.parse(toml)
 }
